5f7ad4c5088d3a0d5d01391e4b3dc97557b327d1
Sarun Kaewwongsa <k.sarun@odds.team>
2026-01-31 15:09:06 +0700

    initial workflow

--- run.sh
+++ run.sh
@@ -3,22 +3,13 @@ set -e
 
 BASE_URL=$1
 PROFILE=$2
-DASHBOARD=$3   # true / false (optional)
-
-if [ -z "$BASE_URL" ] || [ -z "$PROFILE" ]; then
-  echo "Usage: ./run.sh <base_url> <profile> [dashboard]"
-  echo "Example: ./run.sh https://quickpizza.grafana.com load true"
-  exit 1
-fi
 
 mkdir -p reports
 
-echo "BASE_URL=$BASE_URL PROFILE=$PROFILE DASHBOARD=$DASHBOARD"
-
-docker compose run --rm --service-ports \
+docker compose run --rm \
+  -e BASE_URL=$BASE_URL \
+  -e PROFILE=$PROFILE \
+  -e K6_WEB_DASHBOARD=true \
+  -e K6_WEB_DASHBOARD_EXPORT=/reports/dashboard.html \
   k6 run performance.js \
-  --summary-export=/reports/summary.json \
-  --env BASE_URL=$BASE_URL \
-  --env PROFILE=$PROFILE \
-  --env K6_WEB_DASHBOARD=true \
-  --env K6_WEB_DASHBOARD_PORT=5665
+  --summary-export=/reports/summary.json

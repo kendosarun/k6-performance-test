a4fce5d047d81cb0a39bbfbf77612d14fa900b7e
Sarun Kaewwongsa <k.sarun@odds.team>
2026-01-31 16:04:08 +0700

    publish report to pages

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ PROFILE=$2
 
 mkdir -p reports
 
-docker compose run --rm \
-  -e BASE_URL=$BASE_URL \
-  -e PROFILE=$PROFILE \
-  -e K6_WEB_DASHBOARD=true \
-  -e K6_WEB_DASHBOARD_EXPORT=/reports/dashboard.html \
+docker compose run --rm --service-ports \
   k6 run performance.js \
-  --summary-export=/reports/summary.json
+  --summary-export=/reports/summary.json \
+  --env BASE_URL=$BASE_URL \
+  --env PROFILE=$PROFILE \
+  --env K6_WEB_DASHBOARD=true \
+  --env K6_WEB_DASHBOARD_PORT=5665
